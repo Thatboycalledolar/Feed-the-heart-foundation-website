24ed7f8d7ffbac2e14c6802473b930cdcfecb099
olar <kaffoolar@gmail.com>
2026-02-25 12:52:20 +0100

    footer link fix

--- src/components/VolunteerWithUsCard.jsx
+++ src/components/VolunteerWithUsCard.jsx
@@ -1,11 +1,12 @@
 import GenBtn from "./GenBtn"
+import { Link } from "react-router-dom";
 
 export default function VolunteerWithUsCard() {
   return (
     <div className="bg-white py-8 w-full rounded-lg flex flex-col items-center justify-center md:max-w-[1200px] ">
       <h1 className="text-[var(--color-primary)] text-2xl font-bold text-center mb-[-25px] p-4">Donate time and <br /> Expertise</h1>
       <p className="text-[var(--color-black)] text-center font-semibold mb-4 text-sm p-4">Volunteer and Partner with us.</p>
-      <GenBtn link="/contact" text="Volunteer with us today"/>
+      <Link to="/contact"><GenBtn text="Volunteer with us today"/></Link>
     </div>
   )
 } 
