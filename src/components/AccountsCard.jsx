9f3d2bdafc3deeda96a34d41413d55f53a46d794
thatboycalledolar <kaffoolar@gmail.com>
2026-08-16 14:59:50 +0100

    done

--- src/components/AccountsCard.jsx
+++ src/components/AccountsCard.jsx
@@ -1,12 +1,18 @@
 
 
 
-export default function AccountsCard({ accName, accNum, bankName }) {
-  return(
-    <div className="bg-gray-100 p-6 md:py-12 px-18 rounded-lg shadow-md flex flex-col justify-center items-center gap-4 w-full md:w-[80%]">
-      <h3 className="text-md flex flex-col md:flex-row md:justify-start md:gap-2 md:text-lg items-center w-full font-regular text-[var(--color-black)]">Account Number: <span className="text-xl font-bold ">{accNum}</span></h3>
-      <p className="text-md text-center flex flex-col md:flex-row md:justify-start md:gap-2 md:text-lg  items-center w-full font-regular text-[var(--color-black)]">Account Name: <span className="text-xl font-bold ">{accName}</span></p>
-      <p className="text-md flex flex-col md:flex-row md:justify-start md:gap-2 md:text-lg items-center w-full font-regular text-[var(--color-black)]">Bank Name: <span className="text-xl font-bold ">{bankName}</span></p>
+export default function AccountsCard({ accName, accNum, bankName, src }) {
+  return (
+    <div className="bg-gray-100 p-6 md:py-12 px-12 rounded-lg shadow-md flex flex-col justify-center items-center gap-4 w-full md:flex-row md:justify-between md:items-center">
+      <div className="flex flex-col  gap-4 mb-16 md:mb-0 py-8">
+        <h3 className="text-md flex flex-col md:flex-row md:justify-start md:gap-2 md:text-lg items-center w-full font-regular text-[var(--color-black)]">Account Number: <span className="text-xl font-medium font-header tracking-tighter text-[var(--color-primary)] text-[28px] ">{accNum}</span></h3>
+        <p className="text-md text-center flex flex-col md:flex-row md:justify-start md:gap-2 md:text-lg  items-center w-full font-regular text-[var(--color-black)]">Account Name: <span className="text-xl font-medium font-header tracking-tighter text-[var(--color-primary)] text-[28px] ">{accName}</span></p>
+        <p className="text-md flex flex-col md:flex-row md:justify-start md:gap-2 md:text-lg items-center w-full font-regular text-[var(--color-black)]">Bank Name: <span className="text-xl font-medium font-header tracking-tighter text-[var(--color-primary)] text-[28px] ">{bankName}</span></p>
+      </div>
+      <div className="w-24 md:w-36 flex-shrink-0">
+        <img src={src} alt="bank logo" className="w-full h-full object-cover rounded-lg " />
+      </div>
+
     </div>
   )
 }
--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -3,26 +3,25 @@ import AccountsCard from "../components/AccountsCard";
 
 export default function Donate() {
   return (
-    <div className="flex flex-col items-center mt-4 mb-30 justify-center gap-12 px-8 md:px-12 w-full md:max-w-[1200px]">
-      <div id="header-wrapper" className="flex flex-col items-center md:gap-4">
-        <h1 className="font-header text-center leading-14 md:leading-20 text-[48px] md:text-[76px] ">what you donate today,
-        creates <span className="text-[var(--color-primary)]">a brighter tomorrow</span></h1>
+    <div className="flex flex-col items-center mt-12 md:mt-16 lg:mt-20 mb-30 justify-center gap-12 px-8 md:px-12 w-full md:max-w-[1200px]">
+      <div id="header-wrapper" className="flex flex-col items-center md:gap-4 mb-12 md:mb-16 lg:mb-20">
+        <h1 className="font-header text-center leading-14 md:leading-20 text-[48px] md:text-[64px] lg:text-[72px] tracking-tighter font-medium mb-4 md:mb-6 lg:mb-8">What you give today <span className="text-[var(--color-primary)]">can change lives Tomorrow</span></h1>
         <p className="text-[var(--color-black)] font-medium text-[14px] text-center md:text-[18px] md:mt-[-10px]">We can’t do this without you, join us in the fight against poverty</p>
       </div>
-      
+
 
       <div id="accounts-wrapper" className="flex flex-col items-center justify-center w-full gap-16">
         <div id="naira-wrapper" className="w-full flex flex-col items-center gap-4">
-           <h2 className="font-header text-[32px] md:text-[42px] text-(--color-primary)">Naira Account Details</h2>
-            <div id="naira-accounts" className="w-full flex flex-col items-center gap-8">
-              <AccountsCard accName="Feed the Heart Foundation" accNum="1220637968" bankName="Zenith Bank " />  
-            </div>
+          <h2 className="font-header text-[32px] md:text-[42px] text-(--color-primary) tracking-tighter font-medium">Naira Account Details</h2>
+          <div id="naira-accounts" className="w-full flex flex-col items-center gap-8">
+            <AccountsCard accName="Feed the Heart Foundation" accNum="1220637968" bankName="Zenith Bank " src="./Zenith-Bank-Logo.webp" />
+          </div>
         </div>
-        
-        
-       
-       
-        
+
+
+
+
+
       </div>
 
     </div>
